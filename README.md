11d743b3cddd1b8245ba306b9d737f3d377bc90c
Leon Anavi <leon@anavi.org>
2018-05-01 17:44:46 +0300

    HTML5 single page for light demo
    HTML5 single page for connecting to MQTT broker
    through a websocket and controlling the colors
    of a red strip connected to ANAVI Light pHAT or
    ANAVI Light Controller based on machine ID.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# anavi-light-demo
-HTML5 single page demo for controlling lights through MQTT
+# ANAVI Light Demo
+
+HTML5 single page demo for controlling RGB LED strip connect to ANAVI Light pHAT or ANAVI Light Controller through the machine to machine protocol MQTT.
+
+# Libraries
+
+The project depends on the following 3rd party open source libraries:
+* Bootstrap 4.1.1
+* jQuery 3.3.1
+* Popper.js 1.14.3
+* Paho (paho.mqtt.javascript) v1.0.3
---
+++ js/demo.js
@@ -0,0 +1,127 @@
+var mqttClient;
+var host = "iot.eclipse.org";
+var port = 443;
+var path = "/ws";
+var workgroup = "workgroup";
+var machineid = "";
+
+function changeColor(red, green, blue)
+{
+  var payload = '{ "color": {"r": '+ red + ', "g":'+ green +', "b":' + blue + ' } }'
+  message = new Paho.MQTT.Message(payload);
+  message.destinationName = machineid+"/action/rgbled";
+  mqttClient.send(message);
+}
+
+function onConnect() {
+  $('#txtInfo').removeClass('d-none');
+  $('#txtHost').text(host);
+  $('#txtPort').text(port);
+  $('#txtMachineId').text(machineid);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
+
+$(document).ready(function() {
+
+    $('#buttonConnect').on('click', function (e) {
+         e.preventDefault();
+         host = $('#host').val();
+         if (0 == host.length)
+         {
+           host = "iot.eclipse.org";
+         }
+
+         port = $('#port').val();
+         if (0 == port.length)
+         {
+           port = 80;
+         }
+
+         workgroup = $('#workgroup').val();
+         if (0 == workgroup.length)
+         {
+           workgroup = "workgroup";
+         }
+
+         path = $('#path').val();
+         if (0 == path.length)
+         {
+           path = "/ws";
+         }
+
+         machineid = $('#machineid').val();
+         if (0 == machineid.length)
+         {
+           console.log("machine id not set");
+           $('#alertConnect').removeClass('d-none');
+           return;
+         }
+
+         console.log(host);
+         console.log(port);
+         console.log(workgroup);
+         console.log(machineid);
+         var mqttClientId = "LightDemo" + Math.floor((Math.random() * 1000) + 1);
+         mqttClient = new Paho.MQTT.Client(host, Number(port), path, mqttClientId);
+
+         // set callback handlers
+         mqttClient.onConnectionLost = onConnectionLost;
+         mqttClient.onMessageArrived = onMessageArrived;
+
+         // connect the client
+         mqttClient.connect({onSuccess:onConnect});
+
+         $('#alertConnect').addClass('d-none');
+         $("#pageConnect").addClass('d-none');
+         $("#pageColorPicker").removeClass('d-none');
+    });
+
+    $('#buttonDisconnect').on('click', function (e) {
+         e.preventDefault();
+         mqttClient.disconnect();
+         // reset old values
+         $('#host').val('');
+         $('#port').val('');
+         $('#workgroup').val('');
+         $('#machineid').val('');
+         $("#pageColorPicker").addClass('d-none');
+         $('#txtInfo').addClass('d-none');
+         $("#pageConnect").removeClass('d-none');
+    });
+
+    $('#buttonColorMax').on('click', function (e) {
+         e.preventDefault();
+         changeColor(255, 255, 255);
+    });
+
+    $('#buttonColorRed').on('click', function (e) {
+         e.preventDefault();
+         changeColor(255, 0, 0);
+    });
+
+    $('#buttonColorGreen').on('click', function (e) {
+         e.preventDefault();
+         changeColor(0, 255, 0);
+    });
+
+    $('#buttonColorBlue').on('click', function (e) {
+         e.preventDefault();
+         changeColor(0, 0, 255);
+    });
+
+    $('#buttonColorOff').on('click', function (e) {
+         e.preventDefault();
+         changeColor(0, 0, 0);
+    });
+});
